592b2f2a13d64569d308d4a7673704d6d7a10915
Justin Coyne <jcoyne@justincoyne.com>
2021-11-19 12:19:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const { ComprehendClient, DetectDominantLanguageCommand }  = require("@aws-sdk/client-comprehend");
 
 exports.handler = async (event) => {
-    console.log("Event is", event)
     const client = new ComprehendClient({});
-    const command = new DetectDominantLanguageCommand({ Text: 'zhe yang zhen hao' });
+    const command = new DetectDominantLanguageCommand({ Text: event.q });
     const command_response = await client.send(command)
-
+    
     const response = {
-        statusCode: 200,
-        body: command_response,
+       statusCode: 200,
+       body: command_response,
     }
     return response
 }
